021ef9290b52b224270307c5b847111052f49a3c
don <misurakalu@gmail.com>
2021-04-07 15:53:36 +1000

    Commit changes

---
+++ Workshop2Q5/Workshop2Q5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Workshop2Q5
+{
+    class MainClass
+    {
+
+        
+        public static void Main(string[] args)
+        {
+            int Count = 0;
+
+            for (int i= 0; i < args.Length; i++)
+            {
+                int Vvalue = int.Parse(args[i]);
+                int j = i - 1;
+
+                while (int j >= 0  int.Parse(args[j]) ) // the comparison counter is entered in the wrong place
+                {
+
+                }
+            }
+        }
+
+    }
+}
